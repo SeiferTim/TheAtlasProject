6b73adff14e070d24b41e759b16b51bc9eb02451
Tim I Hely <seifertim@gmail.com>
2013-11-18 20:32:32 -0600

    Fixing user/pass for DB

--- js/dbconnection.php
+++ js/dbconnection.php
@@ -1,8 +1,8 @@
 <?php
 	$hostname = "timsworld.db";
 	$database = "atlas";
-	$username = "SeiferTim";
-	$password = "vulpix42";
+	$username = "atlas_user";
+	$password = "atlaspass";
 	$DBconn = mysql_connect($hostname, $username, $password) or trigger_error(mysql_error(),E_USER_ERROR);
 	
 ?>
